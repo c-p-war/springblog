4bf690388da011ae97e43c1298ed4c1d2218cc8f
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-02-26 07:50:00 -0600

    updated forms for edit, added email capability, and adjusted post controller

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -9,83 +9,68 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class PostController {
-    //Dependency injections
+
     private final PostRepository postsDao;
     private final UserRepository usersDao;
 
-    //Nec. construct
-    public PostController (PostRepository postsDao, UserRepository usersDao){
+    public PostController(PostRepository postsDao, UserRepository usersDao){
         this.postsDao = postsDao;
         this.usersDao = usersDao;
     }
 
     @GetMapping("/posts")
-    public String postsIndex(Model model){
-        model.addAttribute("posts", postsDao.findAll());
+    public String postsIndex(Model model) {
 
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String postView(Model model, @PathVariable long id) {
+//        get single post by id later
         Post post = postsDao.getOne(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
 
     @GetMapping("/posts/{id}/edit")
-    public String vieweditPostForm(@PathVariable long id, Model model){
+    public String viewEditPostForm(@PathVariable long id, Model model) {
         model.addAttribute("post", postsDao.getOne(id));
         return "posts/edit";
     }
 
-    ////////////////////////////////////////////////////////
-    //Edit and delete using post mapping for forms (posts)//
-    ////////////////////////////////////////////////////////
     @PostMapping("/posts/{id}/edit")
-    public String updatePost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
-        Post post = new Post(
-                id,
-                title,
-                body
-        );
+    public String updatePost(@PathVariable long id, @ModelAttribute Post post) {
+        //TODO: Change user to logged in user dynamic
+        User user = usersDao.findAll().get(0);
+        post.setUser(user);
         postsDao.save(post);
         return "redirect:/posts";
     }
 
     @PostMapping("/posts/{id}/delete")
     public String deletePost(@PathVariable long id){
-        System.out.println("Removing post");
+        System.out.println("Deleting post...");
         postsDao.deleteById(id);
-        //////////////////////////////////////////////////
-        //Is this part of spring? Or simply a template? //
-        /////////////////////////////////////////////////
         return "redirect:/posts";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String postForm(){
-        return "Create a post here!";
+    public String postForm(Model model){
+        model.addAttribute("post", new Post());
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String createPost(@RequestParam String title, @RequestParam String body){
-        Post post = new Post();
-        post.setTitle(title);
-        post.setBody(body);
-
-        //Needs user in DB
+    public String createPost(@ModelAttribute Post post) {
+        // Will throw if no users in the db!
+        // In the future, we will get the logged in user
         User user = usersDao.findAll().get(0);
-        //getting and setting user who created post
         post.setUser(user);
 
         postsDao.save(post);
-        return "redirect:/posts/"+ post.getId();
+        return "redirect:/posts";
     }
 }
